60e71e625269cfee416ed013050bc253e4c4f092
Quinton H Brown <quintonbrown8101@yahoo.com>
2020-06-23 13:59:49 -0400

    form update

--- src/ContactForm/ContactForm.js
+++ src/ContactForm/ContactForm.js
@@ -12,7 +12,8 @@ const ContactForm = () => {
             <form name="contactform" method="POST" data-netlify-recaptcha="true" data-netlify="true">
                 <div className="logo">
                     <img src="img/logo.png" alt="" />
-                </div>
+                            </div>
+                            <input type="hidden" name="form-name" value="contact" />
                 <input type="text" name="name" placeholder="Enter first and last name" required/>
                 <input type="text" name="email" placeholder="Username or email" required />
                             <textarea className="textarea" type="text" name="message" placeholder="Leave us a message :)" required />
