986463e065a5cd355186d9b0174eb317131e41a3
Quinton H Brown <quintonbrown8101@yahoo.com>
2020-06-23 13:09:28 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ContactForm from '../src/ContactForm/ContactForm.js'
+
+class App extends React.Component {
+  render(){
+    return (
+    <ContactForm />
+  );
+  }
+}
+
+export default App;
---
+++ src/ContactForm/ContactForm.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './ContactForm.css'
+
+
+class ContactForm extends React.Component {
+    render() {
+        return (
+         
+            <div id="contact-wrapper">
+                <div className="contact-container">
+        <div className="contact-form-data">
+            <form action="POST" data-netlify="true">
+                <div className="logo">
+                    <img src="img/logo.png" alt="" />
+                </div>
+                <input type="text" placeholder="Enter first and last name" required/>
+                <input type="text" placeholder="Username or email" required />
+                <textarea className= "textarea" type="text" placeholder="Leave us a message :)" required/>
+                            <button className="contact-form-btn" type="submit">Submit</button>
+                            <div data-netlify-recaptcha="true"></div>
+               
+            
+           
+                        </form>
+                       
+    </div>
+            </div>
+           
+            </div>
+                
+          
+        )
+    }
+}
+export default ContactForm
