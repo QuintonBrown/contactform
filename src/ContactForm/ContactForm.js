e7ca1004801f8870ee7e9897ca07ad8e912b0ec4
Quinton H Brown <quintonbrown8101@yahoo.com>
2020-06-23 14:25:30 -0400

    final

--- src/ContactForm/ContactForm.js
+++ src/ContactForm/ContactForm.js
@@ -9,7 +9,7 @@ const ContactForm = () => {
             <div id="contact-wrapper">
                 <div className="contact-container">
         <div className="contact-form-data">
-            <form name="contactform" method="POST" data-netlify-recaptcha="true" data-netlify="true">
+            <form name="contactform" method="POST" data-netlify="true">
                 <div className="logo">
                     <img src="img/logo.png" alt="" />
                             </div>
@@ -17,7 +17,6 @@ const ContactForm = () => {
                 <input type="text" name="name" placeholder="Enter first and last name" required/>
                 <input type="text" name="email" placeholder="Username or email" required />
                             <textarea className="textarea" type="text" name="message" placeholder="Leave us a message :)" required />
-                            <div data-netlify-recaptcha="true"></div>
                             <button className="contact-form-btn" type="submit">Submit</button>
                             
             
